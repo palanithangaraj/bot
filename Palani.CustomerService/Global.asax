d1cb5bb76805d3728edb3ed23c3d7e618da2cff5
palani Thangaraj <palanithangaraj@yahoo.com>
2019-03-01 09:51:24 -0800

    3rd commit

---
+++ Palani.CustomerService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Palani.CustomerService.WebApiApplication" Language="C#" %>
---
+++ Palani.CustomerService/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+namespace Palani.CustomerService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Conversation.UpdateContainer(
+            builder =>
+            {
+                builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+                var store = new TableBotDataStore(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+                builder.Register(c => store)
+                   .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                   .AsSelf()
+                   .SingleInstance();
+            });
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
