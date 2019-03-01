0c32ea1c7d0b00890673d9900fc92d65a5976d9f
palani Thangaraj <palanithangaraj@yahoo.com>
2019-03-01 09:32:12 -0800

    Second commit

---
+++ Palani.CustomerService/models/Facebook/FacebookLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palani.CustomerService.Models.Facebook
+{
+    public class FacebookLocation
+    {
+        public string Title;
+        public string Longitude;
+        public string Latitude;
+    }
+}
